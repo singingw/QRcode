f94e6cce3bb90043bc3f06703db037afb95a0f3b
singingw <op0408io1998@gmail.com>
2023-03-27 23:50:16 +0800

    hihi

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-const express = require('express')
-/**const exphbs = require('express-handlebars')
-const qr = require('qrcode')**/
-const app = express()
-const port = 3000
-
-/**app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-app.use(express.urlencoded({ extended: true }))**/
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-/**app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/', (req, res) => {
-  const options = req.body
-  const data = `${options.name}\n${options.address}`
-
-  qr.toDataURL(data, (err, url) => {
-    if (err) throw err;
-    res.render('index', { qrCode: url, options: options })
-  })
-})**/
-
-app.listen(port, () => {
-  console.log(`Express app listening on port ${port}.`)
-})
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});

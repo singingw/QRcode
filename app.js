d1f2d0190c48434e9fd1094aa8e487c35bd2bcde
singingw <op0408io1998@gmail.com>
2023-03-28 00:06:12 +0800

    恢復

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const qr = require('qrcode')
 const app = express()
 const port = process.env.PORT || 8080
 
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  const data = `${options.name}\n${options.address}`
+
+  qr.toDataURL(data, (err, url) => {
+    if (err) throw err;
+    res.render('index', { qrCode: url, options: options })
+  })
 })
 
 app.listen(port, () => {

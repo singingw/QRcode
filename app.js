b33f6043ad819c43d502f5e1bd6e42e30af599bb
singingw <op0408io1998@gmail.com>
2023-03-28 00:02:52 +0800

    heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
-});
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})

928dfd6b5ffbe472e8c87b98150255d6a2deefd5
singingw <op0408io1998@gmail.com>
2023-03-28 15:06:29 +0800

    加 express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 8080
@@ -8,6 +9,12 @@ require('./config/mongoose')
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 

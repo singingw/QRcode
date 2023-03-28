3fd81e1555cb2239d494a493509e34274ecce78b
singingw <op0408io1998@gmail.com>
2023-03-28 16:52:47 +0800

    給使用者提示

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 module.exports = {
   authenticator: (req, res, next) => {
     if (req.isAuthenticated()) {
+      req.flash('warning_msg', '請先登入才能使用！')
       return next()
     }
     res.redirect('/users/login')

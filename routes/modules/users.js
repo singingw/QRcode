d24846b88cc1ea90cef1e8623fb07c0e35a8cdd2
singingw <op0408io1998@gmail.com>
2023-03-28 15:39:30 +0800

    新增登出路由

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -40,4 +40,10 @@ router.post('/register', (req, res) => {
   })
   .catch(err => console.log(err))
 })
+router.get('/logout', (req, res) => {
+  req.logout( (err) =>{
+    if (err) { return next(err) }
+    res.redirect('/users/login')
+  })
+})
 module.exports = router
